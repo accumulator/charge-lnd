d40cebfad687f92a5bfaa434a0d117f9c12e0d73
Sander van Grieken <sander@outrightsolutions.nl>
2022-04-21 09:47:14 +0200

    version 0.2.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
     name='charge-lnd',
-    version='0.2.10',
+    version='0.2.11',
     description='A simple policy based fee manager for LND',
     author='Sander van Grieken',
     author_email='sander@outrightsolutions.nl',
