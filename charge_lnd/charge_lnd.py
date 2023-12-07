06982ad6db4857b73b831fe8228a969e92381947
Tim Horie <tak@thorie.com>
2023-12-07 17:55:50 +0900

    Remove debugging

--- charge_lnd/charge_lnd.py
+++ charge_lnd/charge_lnd.py
@@ -123,8 +123,6 @@ def main():
                 if time_lock_delta_changed:
                     s = ' ➜ ' + fmt.col_hi(new_time_lock_delta)
                 print("  time_lock_delta: %s%s" % (fmt.col_hi(my_policy.time_lock_delta), s) )
-        #debugging - just exit after one
-        return True
 
     return True
 
